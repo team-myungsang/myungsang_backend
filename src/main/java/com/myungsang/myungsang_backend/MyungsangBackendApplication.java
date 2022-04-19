7f2c554348779e282e303d9265576da698fae742
sanghyupjung <54101618+sanghyupjung@users.noreply.github.com>
2022-04-19 23:21:50 +0900

    Add files via upload
    initialize myungsang backend project

---
+++ src/main/java/com/myungsang/myungsang_backend/MyungsangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myungsang.myungsang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyungsangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyungsangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myungsang/myungsang_backend/MyungsangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myungsang.myungsang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyungsangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
