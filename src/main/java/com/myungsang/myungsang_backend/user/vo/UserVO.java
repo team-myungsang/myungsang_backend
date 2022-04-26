313b094b5e4a6c05a1b6b014efb79b33842a5eda
nohkwanok <cloq@kakao.com>
2022-04-26 23:00:07 +0900

    getUsers 매핑

---
+++ src/main/java/com/myungsang/myungsang_backend/user/vo/UserVO.java
@@ -0,0 +1,45 @@
+package com.myungsang.myungsang_backend.user.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserVO {
+    private long id;
+    private String email;
+    private String name;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
