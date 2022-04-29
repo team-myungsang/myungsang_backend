fe592846425530e960d70c6ff4862cfbb943981e
nohkwanok <cloq@kakao.com>
2022-04-29 22:14:52 +0900

    video get one

---
+++ src/main/java/com/myungsang/myungsang_backend/video/VideoController.java
@@ -0,0 +1,24 @@
+package com.myungsang.myungsang_backend.video;
+
+import com.myungsang.myungsang_backend.video.iservice.VideoIService;
+import com.myungsang.myungsang_backend.video.vo.VideoVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController {
+
+    @Autowired
+    public VideoController(VideoIService videoIService) {
+        this.videoIService = videoIService;
+    }
+
+    VideoIService videoIService;
+
+    @GetMapping("videos/{id}")
+    public VideoVo getVideo(@PathVariable long id) {
+        return videoIService.getVideo(id);
+    }
+}
