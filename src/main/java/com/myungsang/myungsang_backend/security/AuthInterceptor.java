05e77817f19e3808d76a6677324c1dff4aa77834
nohkwanok <cloq@kakao.com>
2022-05-15 20:09:25 +0900

    error page pass

--- src/main/java/com/myungsang/myungsang_backend/security/AuthInterceptor.java
+++ src/main/java/com/myungsang/myungsang_backend/security/AuthInterceptor.java
@@ -39,6 +39,7 @@ public class AuthInterceptor implements HandlerInterceptor {
         String requestUri = request.getRequestURI();
 
         if(requestUri.equals("/login")
+                || requestUri.equals("/error")
                 || requestUri.equals("/logout")
                 || requestUri.equals("/register")
                 || requestUri.equals("/main/videos")
